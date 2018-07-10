8139a4385813a854443a14314afa9070a4a9ac03
Anis Lucidi <anislucidi@gmail.com>
2018-07-10 15:34:09 +0200

    Added free aiming
    Added free aiming script, changed aiming animation, although it still isn't right for the player, need to find a new one or adapt it

---
+++ TPS Project/Assets/Asset Test/Scripts/Player/AimingFreely.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimingFreely : MonoBehaviour {
+
+     Animator Anim;
+    public Camera camera;
+    public Vector3 offSet;
+    Transform chest;
+	// Use this for initialization
+	void Start () {
+        Anim = GetComponent<Animator>();
+        chest = Anim.GetBoneTransform(HumanBodyBones.Chest);
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+	}
+   
+
+    // Update is called once per frame
+    void LateUpdate() {
+
+        if (Anim.GetCurrentAnimatorStateInfo(0).IsName("Aim"))
+            {
+
+
+            chest.transform.rotation = new Quaternion(camera.transform.rotation.x, transform.rotation.y, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
+        }
+    }
+}
