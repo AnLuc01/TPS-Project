d3642e1378f309068e94130f64d221b7d289e154
Anis Lucidi <anislucidi@gmail.com>
2018-07-19 02:21:20 +0200

    Added aiming for NPC
    REMINDER NEED TO MAKE IT AIM FREELY

--- TPS Project/Assets/Asset Test/Scripts/Player/AimingFreely.cs
+++ TPS Project/Assets/Asset Test/Scripts/Player/AimingFreely.cs
@@ -6,6 +6,7 @@ public class AimingFreely : MonoBehaviour {
 
      Animator Anim;
     public Camera camera;
+    public GameObject Target;
     public Quaternion offSet;
     Transform chest;
     public Transform hand;
@@ -19,19 +20,25 @@ public class AimingFreely : MonoBehaviour {
 
     // Update is called once per frame
     void LateUpdate() {
-
-        if (!PauseMenuScript.gameIsPaused)
+        if (name == "Player")
         {
-
-            if (Anim.GetCurrentAnimatorStateInfo(0).IsName("Aim"))
+            if (!PauseMenuScript.gameIsPaused)
             {
 
+                if (Anim.GetCurrentAnimatorStateInfo(0).IsName("Aim"))
+                {
 
-                chest.transform.rotation = new Quaternion(camera.transform.rotation.x, transform.rotation.y, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
-                hand.transform.rotation = offSet;
-                Anim.GetBoneTransform(HumanBodyBones.RightHand).transform.rotation = offSet;
+
+                    chest.transform.rotation = new Quaternion(camera.transform.rotation.x, transform.rotation.y, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
+                    hand.transform.rotation = offSet;
+                    Anim.GetBoneTransform(HumanBodyBones.RightHand).transform.rotation = offSet;
+                }
             }
         }
-        
+        else
+        {
+            chest.transform.rotation = new Quaternion(Target.transform.rotation.x, transform.rotation.y, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
+
+        }
     }
 }
---
+++ TPS Project/Assets/Asset Test/Scripts/Player/NPC/NPCShoot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCShoot : MonoBehaviour {
+    public Animator NPCAnim;
+    public Transform target;
+    public Transform Chest;
+    Rigidbody NPCRigid;
+    public Transform Hand;
+    public float offSet;
+    public Quaternion ChestRot;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(NumberGen());
+        NPCRigid = GetComponent<Rigidbody>();
+        NPCAnim = GetComponent<Animator>();
+        Chest = NPCAnim.GetBoneTransform(HumanBodyBones.Chest);
+        Hand = NPCAnim.GetBoneTransform(HumanBodyBones.LeftHand);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        NPCRigid.constraints = RigidbodyConstraints.FreezeRotationZ| RigidbodyConstraints.FreezeRotationX;
+        Aim();
+	}
+    IEnumerator NumberGen()
+    {
+        while (true)
+        {
+            offSet = Random.Range(-0.5f, 0.5f);
+            yield return new WaitForSeconds(2);
+        }
+    }
+
+
+    void Aim()
+    {
+        NPCAnim.SetBool("Aiming", true);
+        NPCAnim.SetBool("Pistol", true);
+        transform.LookAt(new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z+offSet));
+        Debug.DrawRay(transform.position, transform.forward, Color.blue);
+    }
+
+}
